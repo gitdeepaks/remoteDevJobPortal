0c8cec0d5a2cbd1bc7db677f015ff324cd47c561
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-11 17:15:52 +0530

    new commit🚀

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,6 +7,7 @@ import {
 } from "../common.js";
 import renderError from "./Error.js";
 import renderSpinner from "./Spinner.js";
+import renderJobList from "./JobList.js";
 
 const submitHandler = (e) => {
   // prevent default behaviour
@@ -50,31 +51,8 @@ const submitHandler = (e) => {
       // render the job list
       numberEl.textContent = jobItems.length;
 
-      // render jonitems in the list
-
-      jobItems.slice(0, 7).forEach((jobItem) => {
-        const newJobItemHTML = `
-          <li class="job-item">
-          <a class="job-item__link" href="${jobItem.id}">
-              <div class="job-item__badge">${jobItem.badgeLetters}</div>
-              <div class="job-item__middle">
-                  <h3 class="third-heading">${jobItem.company}</h3>
-                  <p class="job-item__company">LakeOperations</p>
-                  <div class="job-item__extras">
-                      <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
-                      <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salary}</p>
-                      <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i> ${jobItem.location}</p>
-                  </div>
-              </div>
-              <div class="job-item__right">
-                  <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
-                  <time class="job-item__time">${jobItem.daysAgo}d</time>
-              </div>
-          </a>
-      </li>`;
-        jobListSearchEl.insertAdjacentHTML("beforeend", newJobItemHTML);
-      });
-      ˀ;
+      // render job items in the list
+      renderJobList(jobItems);
     })
     .catch((err) => {
       console.log(err);
